2ecc32fdb5db4bc05c020cb57db7b494021125af
Tianomiano <patomiano6@gmail.com>
2022-10-15 11:54:28 -0700

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints all single digits to the base 10
+*Return: 0
+*/
+
+int main(void)
+{
+	int single;
+
+	for (single = 0; single < 10; single++)
+	{
+		putchar((single % 10) + '0');
+	}
+	putchar("\n");
+	return (0);
+}
