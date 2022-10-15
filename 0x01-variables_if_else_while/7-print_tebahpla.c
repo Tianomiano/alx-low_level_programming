ec756d988676cdfaf8245b41e51a1b6a89003809
Tianomiano <patomiano6@gmail.com>
2022-10-15 12:04:45 -0700

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <srdlib.h>
+
+/**
+*main - alphabets in reverse
+*Return: 0
+*/
+
+int main(void)
+{
+	char as;
+
+	for (as = 'z'; a >= 'a'; as--)
+	{
+		putchar(as);
+	}
+	putchar('\n');
+	return (0);
+}
