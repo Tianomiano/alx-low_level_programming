db14dd5ab418b707f88ed23ac3e08f70559dabd8
Tianomiano <patomiano6@gmail.com>
2022-10-15 12:17:38 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - numbers to base 16
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
