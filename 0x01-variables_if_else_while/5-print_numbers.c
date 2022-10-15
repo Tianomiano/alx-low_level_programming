8472359f98da961f48b445d5f9812155da841d3c
Tianomiano <patomiano6@gmail.com>
2022-10-15 11:33:37 -0700

    10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints numbers to the base 10
+*Return: 0
+*/
+
+inta main(void)
+{
+	int i;
+
+	i = 0;
+	for (i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+	return (0);
+}
