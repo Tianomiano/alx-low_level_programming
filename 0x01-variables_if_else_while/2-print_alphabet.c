36188791c4e27044d193f7ddb0b649b42cc53f25
Tianomiano <patomiano6@gmail.com>
2022-10-15 10:17:47 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints alphabet in lowercase
+*Return: 0
+*/
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++);
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
