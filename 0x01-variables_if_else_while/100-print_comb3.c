a26994dbafd9d2bc76ecff3e6b1986079edcb942
Tianomiano <patomiano6@gmail.com>
2022-10-16 10:54:03 -0700

    2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - combination of two digits
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; j = 0; j < 100; i++; j++)
+	{
+		if (i != j)
+		{
+			putchar(ij);
+		}
+		if (i != 57 && j != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
