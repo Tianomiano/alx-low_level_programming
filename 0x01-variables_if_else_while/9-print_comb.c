6dc1c3c559c2e071b9c8b6ea0cb4f554ebe3140e
Tianomiano <patomiano6@gmail.com>
2022-10-16 09:58:04 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints combination of single digits
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
