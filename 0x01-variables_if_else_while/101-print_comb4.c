3443983ea16cb97ad2e1422cdea7606b8d200444
Tianomiano <patomiano6@gmail.com>
2022-10-17 01:02:14 -0700

    3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints unique combinations of three digits
+*Return: 0
+*/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 56; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (k = 48; k < 58)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k < 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
