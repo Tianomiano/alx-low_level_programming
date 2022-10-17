302d465c3dadabd1ba48721b32328754f495dd0c
Tianomiano <patomiano6@gmail.com>
2022-10-17 01:28:53 -0700

    3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,16 +12,16 @@ int main(void)
 
 	for (i = 48; i < 56; i++)
 	{
-		for (j = 48; j < 57; j++)
+		for (j = i + 1; j < 57; j++)
 		{
-			for (k = 48; k < 58; k++)
+			for (k = j + 1; k < 58; k++)
 			{
 				if (i != j && i != k && j != k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i <56 && j <57 && k < 58)
+					if (i < 56 && j < 57 && k < 58)
 					{
 						putchar(',');
 						putchar(' ');
