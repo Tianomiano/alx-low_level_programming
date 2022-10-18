f4c11b80118eb34ec4f0e77bccf9c5f87ac1204e
Tianomiano <patomiano6@gmail.com>
2022-10-18 11:29:26 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*jack_bauer - prints every minute of the day
+*
+*Return: 0
+*/
+
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 60; minutes++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
