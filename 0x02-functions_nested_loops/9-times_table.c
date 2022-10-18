4b8360051d56cefd7fdf077a9224f4223909bc97
Tianomiano <patomiano6@gmail.com>
2022-10-18 12:50:06 -0700

    times 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*times_table - prints timetable of 9
+*Return: 0
+*/
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int mult;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar('0');
+		for (b = 1; b < 10; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			mult = a * b
+			if (mult <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
+		}
+	}
+}
