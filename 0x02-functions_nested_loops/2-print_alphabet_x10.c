b73b2eb99f7910c799ae8b8fde9cff63d70ba80c
Tianomiano <patomiano6@gmail.com>
2022-10-18 09:37:58 -0700

    alpha 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints alphabet in lowercase 10 times
+*Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	i++;
+	}
+}
