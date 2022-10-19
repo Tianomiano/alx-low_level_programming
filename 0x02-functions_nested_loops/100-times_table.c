918185e015ccc845df395f4c9f8c67f48492ccf6
Tianomiano <patomiano6@gmail.com>
2022-10-19 00:06:23 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,6 +40,10 @@ void print_times_table(int n)
 					_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
 				}
+				else if (c <= 9)
+				{
+					_putchar(c % 10 + '0');
+				}
 			}
 			_putchar('\n');
 		}
