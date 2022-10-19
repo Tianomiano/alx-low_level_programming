025ed23d3f3b65f1785c33056f3849423f1c3664
Tianomiano <patomiano6@gmail.com>
2022-10-19 00:16:25 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,9 +23,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				c = a * b;
 				if (c <= 99)
-				{
 					_putchar(' ');
-				}
 				if (c <= 9)
 				{
 					_putchar(' ');
@@ -33,7 +31,8 @@ void print_times_table(int n)
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
-					_putchar(((c / 10)) % 10 + '0');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
 				}
 				else if (c <= 99 && c >= 10)
 				{
