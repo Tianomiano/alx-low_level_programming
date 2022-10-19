9e9a3f9f5a6d6a31f16a0c50108ebc6124ada769
Tianomiano <patomiano6@gmail.com>
2022-10-19 02:29:20 -0700

    98 fibbo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - prints fibbonacci
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long j = 1;
+	unsigned long k = 2;
+	unsigned long l;
+
+	for (i = 0; i < 98; i++)
+	{
+		l = j + k;
+		printf("%lu", l);
+		j = k;
+		k = l;
+		if (i == 97)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
