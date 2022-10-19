2ac169bacc33cb94f9fe26a336562ce1b7d33de4
Tianomiano <patomiano6@gmail.com>
2022-10-19 01:51:39 -0700

    fibbo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - prints fibonacci sequence
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long j = 0;
+	unsigned long k = 1;
+	unsigned long l;
+
+	for (i = 0; i < 50)
+	{
+		l = j + k;
+		printf("%lu", l);
+		j = k;
+		k = l;
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
