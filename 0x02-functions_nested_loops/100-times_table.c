ef1b4a28fa5fb62c768c10713270fc449e2a685b
Tianomiano <patomiano6@gmail.com>
2022-10-18 23:17:14 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,35 +8,40 @@
 
 void print_times_table(int n)
 {
-	int m;
+	int a;
+	int b;
+	int c;
 
 	for (n = 0; n <= 15; n++)
 	{
-		_putchar('0');
-		for (n = 1; n <= 15; n++)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			m = n * n;
-			if (m <= 99)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
+				_putchar(',');
 				_putchar(' ');
+				c = a * b;
+				if (c <= 99)
+				{
+					_putchar(' ');
+				}
+				if (c <= 9)
+				{
+					_putchar(' ');
+				}
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar((c % 100) + '0');
+				}
+				else if (c <= 99 && c >= 10)
+				{
+					_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+				}
 			}
-			if (m <= 9)
-			{
-				_putchar(' ');
-			}
-			if (m >= 100)
-			{
-				_putchar((m / 100) + '0');
-				_putchar((m % 100) + '0');
-			}
-			else if (m <= 99 && m >= 10)
-			{
-				_putchar(m / 10 + '0');
-			_putchar(m % 10 + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
