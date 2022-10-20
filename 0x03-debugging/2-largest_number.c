302381c0e83206c16f857b11a1c169aa24a20dcc
Tianomiano <patomiano6@gmail.com>
2022-10-20 11:18:13 -0700

    large

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,9 +12,6 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -23,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
