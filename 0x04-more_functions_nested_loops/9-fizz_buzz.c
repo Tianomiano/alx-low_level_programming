2a3cabbc809e36b1a95702a19d7061a7a1c5d0f9
Tianomiano <patomiano6@gmail.com>
2022-10-21 12:00:04 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - prints 1 to 100
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i  = 1; i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
