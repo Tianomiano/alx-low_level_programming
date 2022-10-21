ec943c6734cee605938378ed28c215aca19ef4f1
Tianomiano <patomiano6@gmail.com>
2022-10-21 05:29:14 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - prints numbers 0-9
+*Return: 0
+*/
+
+void print_numbers(void)
+{
+	int l;
+
+	for (l = '0'; l <= '9')
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+	return (0);
+}
