96b5f45a7af9f56ddb9730b8ad067d7b9f355b63
Tianomiano <patomiano6@gmail.com>
2022-10-21 05:13:49 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isdigit - checks for digits 0 through 9
+*@c: Digit checked
+*Return: 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
