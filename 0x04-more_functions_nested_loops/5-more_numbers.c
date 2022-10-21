2b2fad733e1426326c92394624bf231567f00f57
Tianomiano <patomiano6@gmail.com>
2022-10-21 06:01:55 -0700

    0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 0-14 10 times
+*Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+		}
+		j++;
+	_putchar('\n');
+	i++;
+	}
+	_putchar('\n');
+}
