e10d435570538355ec67b480e8d25055a525a199
Tianomiano <patomiano6@gmail.com>
2022-10-25 11:19:55 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*rev_string - reverses a string
+*@s: string
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int j = 0;
+	char c = s[0];
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		j--;
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
