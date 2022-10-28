20b6f6f7796ed3290a13803dfc96b324a2119d29
Tianomiano <patomiano6@gmail.com>
2022-10-28 11:36:10 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*reverse_array - reverses an array
+*@a: array
+*@n: number of elements
+*Return: reverse array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = a[j];
+	}
+}
