8c6aca49d2a27bb10dadc3efd57171f6f0e4ec86
Tianomiano <patomiano6@gmail.com>
2022-10-28 12:53:52 -0700

    replace

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+**leet - encodes a a string
+*@c: string
+*Return: char
+*/
+
+char *leet(char *c)
+{
+	int i;
+	int j;
+	char s1[] = "aAeEoOtTlL"
+	char s2[] = "4433007711"
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == s1[j])
+			{
+				c[i] = s2[j];
+			}
+		}
+	}
+	return (c);
+}
