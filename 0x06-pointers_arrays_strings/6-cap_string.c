3dad04a943db9b89d196b9745e19847616895c95
Tianomiano <patomiano6@gmail.com>
2022-10-28 12:32:42 -0700

    upp

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,39 @@
 
 /**
 *cap_string - capitalizes strings
-*@:
+*@c: string
 *Return: char
 */
 
-char *cap_string(char *)
+char *cap_string(char *c)
 {
+	int i;
+
+	i = 0;
+	while (c[i])
+	{
+		while (!c[i] >= 'a' && c[i] <= 'z')
+		{
+			i++;
+		}
+		if (c[i - 1] == ' ' ||
+		c[i - 1] == '\t' ||
+		c[i - 1] == '\n' ||
+		c[i - 1] == ',' ||
+		c[i - 1] == ';' ||
+		c[i - 1] == '.' ||
+		c[i - 1] == '!' ||
+		c[i - 1] == '?' ||
+		c[i - 1] == '"' ||
+		c[i - 1] == '(' ||
+		c[i - 1] == ')' ||
+		c[i - 1] == '{' ||
+		c[i - 1] == '}' ||
+		i == 0)
+		{
+			c[i] -= 32;
+		}
+		i++;
+	}
+	return (c);
+}
