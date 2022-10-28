ff121bad4490dd59d2a3d2e34f6c9f6d13cd8328
Tianomiano <patomiano6@gmail.com>
2022-10-28 11:46:43 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+**string_toupper - lower to upper
+*@c: pointer
+**Return: char
+*/
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] >= 'a' && c[i] <= 'z')
+	{
+		c[i] = c[i] - 32;
+	i++;
+	}
+	return (c);
+}
