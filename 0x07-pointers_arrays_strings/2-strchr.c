abc03a0239a8528ee78b702110d1382f7588fb7d
Tianomiano <patomiano6@gmail.com>
2022-11-01 11:43:15 -0700

    char c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,17 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int i = 0, j;
+
+	while (s[a])
 	{
-		if (*s != c)
-			s++;
-		else
+		a++;
+	}
+	for (b = 0; b < n; b++)
+	{
+		if (c == s[b])
+			s += b
 			return (s);
 	}
-	if (c == '\0')
-		return (s);
-	return (NULL);
+	return ('\0');
 }
