e860a8c4e6c761e6b4a651d22caac0a75fbf7efd
Tianomiano <patomiano6@gmail.com>
2022-11-01 11:16:03 -0700

    char c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strchr - locates character in a string
+*@s: string
+*@c: char
+*Return: pointer to char
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s);
+		}
+		if (c == '\0')
+		{
+			return (s);
+		}
+		return (NULL);
+	}
+}
