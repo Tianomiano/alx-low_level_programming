52d979c51c9261755ae2823e9044d2f2aa17925c
Tianomiano <patomiano6@gmail.com>
2022-11-01 23:43:05 -0700

    diagon

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 *print_diagsums - sum of two diagonals
 *@a: int
@@ -8,4 +9,20 @@
 
 void print_diagsums(int *a, int size)
 {
+	int in;
+	int sum1 = 0;
+	int sum2 = 0;
 
+	for (in = 0; in < size; in++)
+	{
+		sum1 += a[size];
+		a += size;
+	}
+	a -= size;
+	for (in = 0; in < size; in++)
+	{
+		sum += a[size];
+		a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
