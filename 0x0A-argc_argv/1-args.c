ef69c6727c0846cd5064547b1197c3b884f97085
Tianomiano <patomiano6@gmail.com>
2022-11-07 09:57:35 -0800

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - prints no of arg passed
+*@argc: number of arg
+*@argv: arguments
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
