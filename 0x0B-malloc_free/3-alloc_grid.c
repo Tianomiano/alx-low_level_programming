e4dedae5016117069845eb30d7589d1ec6605376
Tianomiano <patomiano6@gmail.com>
2022-11-08 06:21:53 -0800

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int *) * height);
+	grid = malloc(sizeof(int) * height);
 	if (grid == NULL)
 	{
 		return (NULL);
@@ -30,11 +30,11 @@ int **alloc_grid(int width, int height)
 			{
 				free(grid[i]);
 			}
-		free(grid);
-		return (NULL);
+			free(grid);
+			return (NULL);
 		}
 	}
-	for (j = 0; j < width; j++)
+	for (i = 0; j < width; j++)
 	{
 		grid[i][j] = 0;
 	}
