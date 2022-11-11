c57095d54269a9e165abb67133b5e663d4b026cd
Tianomiano <patomiano6@gmail.com>
2022-11-11 06:15:10 -0800

    allocate

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*malloc_checked - allocates memory using malloc
+*@b: allocated memory
+*Return: pointer to memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *all
+
+	all = malloc(b)
+	if (malloc == NULL)
+	{
+		exit(98);
+	}
+	return (all);
+}
