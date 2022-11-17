4192449814d4f6987dd5a1288215c86d5a409238
Tianomiano <patomiano6@gmail.com>
2022-11-17 02:34:58 -0800

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+
+/**
+*int_index - returns index
+*@array: array
+*@cmp: pointer to func that compares values
+*@size: number of elements in array
+*Return: 0,1 or -1
+*
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			i = 0;
+			while (i < size)
+			{
+				if (cmp(array[i]))
+				{
+					return (i);
+				i++;
+				}
+				else
+					return (-1);
+			}
+		}
+	}
+}
