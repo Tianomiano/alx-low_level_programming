70aa45c7d62739e1c68324d4f6b7dbdc3d4fdbf9
Tianomiano <patomiano6@gmail.com>
2022-11-30 22:59:52 -0800

    op

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,17 @@
 /**
 *print_listint - prints all elements in a list
 *@h: head of list
-*Return: elemnt count
+*Return: element count
 */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		count++;
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
