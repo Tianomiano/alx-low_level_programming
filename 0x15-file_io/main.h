6b66c46791b25157df465e38868b777dc1eed11a
Tianomiano <patomiano6@gmail.com>
2022-12-07 23:16:03 +0300

    o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
